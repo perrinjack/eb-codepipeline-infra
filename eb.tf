c818abac7af9d13b83c8005259243ef7e51cc2bb
Jack Perrin <perrinjack96@gmail.com>
2024-03-22 19:42:30 +0000

    Use correct image for codebuild

--- eb.tf
+++ eb.tf
@@ -3,7 +3,7 @@ resource "aws_elastic_beanstalk_application" "app" {
 }
 
 resource "aws_elastic_beanstalk_environment" "development" {
-  name                = "${var.app_name}-development-environment"
+  name                = "${var.app_name}-env"
   application         = aws_elastic_beanstalk_application.app.name
   solution_stack_name = "64bit Amazon Linux 2023 v6.1.1 running Node.js 20"
 }
